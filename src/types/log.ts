70c0300937b35875fda6844ad9d963604e223b80
zelalemyetsedaw <zy94988@gmail.com>
2024-10-26 14:50:24 -0300

    Update log.ts

--- src/types/log.ts
+++ src/types/log.ts
@@ -1,3 +1,4 @@
+// src/types/log.ts
 export interface LogRecord {
     timeUnixNano: string;
     observedTimeUnixNano: string;
@@ -6,12 +7,24 @@ export interface LogRecord {
     body: {
       stringValue: string;
     };
-    attributes: Attribute[];
+    attributes: Attribute[]; // Allow multiple types here
+    droppedAttributesCount: number;
+  }
+  
+  export interface Log {
+    timeUnixNano: string;
+    observedTimeUnixNano: string;
+    severityNumber: number;
+    severityText: string;
+    body: {
+      stringValue: string;
+    };
+    attributes: Attribute[]; // Match the type to LogRecord
     droppedAttributesCount: number;
   }
   
   export interface Attribute {
     key: string;
-    value: string | number | boolean | Record<string, unknown> | null;
+    value: string | number | boolean | Record<string, unknown> | null; // Accept multiple types
   }
   
